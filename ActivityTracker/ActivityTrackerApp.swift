8272327a4ee2b761dd23777bdfbf57524bb77dd8
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-07-19 15:01:17 -0400

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Djallil Elkebir on 2021-07-19.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
