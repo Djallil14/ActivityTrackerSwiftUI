a4a6951398f2e0bde30ffd25c03b6c8206b98033
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-07-20 00:12:39 -0400

    Made HomeView usable and more user friendly

---
+++ ActivityTracker/Extensions/Colors+Addition.swift
@@ -0,0 +1,13 @@
+//
+//  Colors+Addition.swift
+//  ActivityTracker
+//
+//  Created by Djallil Elkebir on 2021-07-19.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
